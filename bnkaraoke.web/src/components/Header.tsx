23fb730122d0068e39b89a972f6710b189d7a212
tstrand@strandcentral.com <12416734+tdstrand@users.noreply.github.com>
2025-08-17 15:17:22 -0500

    Update web app mobile to see events in header - update header height on mobile -13

--- bnkaraoke.web/src/components/Header.tsx
+++ bnkaraoke.web/src/components/Header.tsx
@@ -1,4 +1,4 @@
-// src/components/Header.tsx
+// src/components/Header.tsx 1
 import React, { useState, useEffect, useRef, useCallback, useMemo, memo } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { LogoutOutlined } from "@ant-design/icons";
