752bf4927c64b5d5c2bd26082896e26b33c8b981
tdstrand <12416734+tdstrand@users.noreply.github.com>
2025-08-24 19:28:19 -0500

    feat: improve mobile queue drag and handle

--- bnkaraoke.web/src/components/SortableItem.tsx
+++ bnkaraoke.web/src/components/SortableItem.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { useSortable } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
+import { HolderOutlined } from '@ant-design/icons';
 import './SortableItem.css';
 
 interface SortableItemProps {
@@ -23,7 +24,16 @@ export const SortableItem: React.FC<SortableItemProps> = ({ id, disabled = false
   };
 
   return (
-    <div ref={setNodeRef} style={style} {...attributes} {...listeners} className={`mobile-sortable-item ${className || ''}`}>
+    <div
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      className={`mobile-sortable-item ${className || ''}`}
+      data-disabled={disabled || undefined}
+    >
+      <span className="drag-handle" {...listeners}>
+        <HolderOutlined />
+      </span>
       {children}
     </div>
   );
