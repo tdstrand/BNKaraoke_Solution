2538b93bf134879680ae3bca3dde38821758296f
tdstrand <12416734+tdstrand@users.noreply.github.com>
2025-10-03 09:22:46 -0500

    Fix overlay band padding XAML

--- BNKaraoke.DJ/Views/Overlays/OverlayBand.xaml.cs
+++ BNKaraoke.DJ/Views/Overlays/OverlayBand.xaml.cs
@@ -1,12 +1,25 @@
+using System.Windows;
 using System.Windows.Controls;
 
 namespace BNKaraoke.DJ.Views.Overlays
 {
     public partial class OverlayBand : UserControl
     {
+        public static readonly DependencyProperty ContentPaddingProperty = DependencyProperty.Register(
+            nameof(ContentPadding),
+            typeof(Thickness),
+            typeof(OverlayBand),
+            new PropertyMetadata(new Thickness(32, 24, 32, 24)));
+
         public OverlayBand()
         {
             InitializeComponent();
+        }
+
+        public Thickness ContentPadding
+        {
+            get => (Thickness)GetValue(ContentPaddingProperty);
+            set => SetValue(ContentPaddingProperty, value);
         }
     }
 }
