94d330d1488ce7b54a33450b6821ba6b399ecea2
tdstrand <12416734+tdstrand@users.noreply.github.com>
2025-10-06 14:45:09 -0500

    Remove unused Shapes namespace causing Path ambiguity

--- BNKaraoke.DJ/Views/VideoPlayerWindow.xaml.cs
+++ BNKaraoke.DJ/Views/VideoPlayerWindow.xaml.cs
@@ -14,7 +14,6 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Interop;
 using System.Windows.Media;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 
 namespace BNKaraoke.DJ.Views
